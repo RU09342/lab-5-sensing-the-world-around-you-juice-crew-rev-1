23c95697c6953ddce331018994e0ce81d0ab1a14
Matthew Dion <dionm9@students.rowan.edu>
2017-11-09 13:11:16 -0500

    Created FR2311 UART Sensor Program

---
+++ Sensors and Signal Conditioning (1)/MSP430F5529/main.c
@@ -0,0 +1,95 @@
+/* --COPYRIGHT--,BSD_EX
+ * Copyright (c) 2014, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************
+ *
+ *                       MSP430 CODE EXAMPLE DISCLAIMER
+ *
+ * MSP430 code examples are self-contained low-level programs that typically
+ * demonstrate a single peripheral function or device feature in a highly
+ * concise manner. For this the code may rely on the device's power-on default
+ * register values and settings such as the clock configuration and care must
+ * be taken when combining code from several examples to avoid potential side
+ * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
+ * for an API functional library-approach to peripheral configuration.
+ *
+ * --/COPYRIGHT--*/
+//******************************************************************************
+
+#include <msp430.h> 
+
+
+
+unsigned int readTemp(void);
+
+int main(void){
+
+    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+
+    //Timer set up
+
+    // uart set up
+    P4SEL = BIT4+BIT5; // P3.4,5 = USCI_A0 TXD/RXD
+    UCA1CTL1 |= UCSWRST; // **Put state machine in reset**
+    UCA1CTL1 |= UCSSEL_2; // SMCLK
+    UCA1BR0 = 6; // 1MHz 9600 (see User's Guide)
+    UCA1BR1 = 0; // 1MHz 9600
+    UCA1MCTL = UCBRS_0 + UCBRF_13 + UCOS16; // Modln UCBRSx=0, UCBRFx=0,
+    // over sampling
+    UCA1CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
+    //configure timer for temp reading every second
+        TA0CTL= (MC_1 + TASSEL__SMCLK + ID__8);//set timer0 to up, SMCLK, no division, DIVIDE BY 8
+        TA0CCTL0 = CCIE;
+        TA0CCR0=100;      //set PWM frequency to 1 Hz
+}
+
+/*ISR FOR TIMERA0 CCR0*/
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer0_A0_ISR (void)
+{
+    while (!(UCA1IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
+    UCA1TXBUF = readTemp();                  // TX -> RXed character
+}
+
+unsigned int readTemp(void){
+    REFCTL0 &= ~REFMSTR;
+    P6SEL |= BIT2;
+    ADC12CTL0 = ADC12SHT0_9+ADC12ON;
+    ADC12CTL1 = ADC12SHP+ADC12CSTARTADD_2;
+    ADC12MCTL2 = ADC12INCH_2;
+    ADC12CTL0 |= ADC12SC+ADC12ENC;
+    while(ADC12CTL1 & ADC12BUSY);
+    __no_operation();
+
+    return ADC12MEM2 & 0xFFF;
+}
+
+
--- Sensors and Signal Conditioning/MSP430F5529/Debug/subdir_rules.mk
+++ Sensors and Signal Conditioning/MSP430F5529/Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 main.obj: ../main.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/Program Files/ccsv7/tools/compiler/ti-cgt-msp430_16.9.3.LTS/bin/cl430" -vmspx --data_model=restricted --use_hw_mpy=F5 --include_path="C:/Program Files/ccsv7/ccs_base/msp430/include" --include_path="C:/Users/IceCreamMachine/Google Drive/Rowan/2017.9 Fall - junior/Embeded/Git Hub/lab-5-sensing-the-world-around-you-juice-crew-rev-1/Sensors and Signal Conditioning/MSP430F5529" --include_path="C:/Program Files/ccsv7/tools/compiler/ti-cgt-msp430_16.9.3.LTS/include" --advice:power=all --define=__MSP430F5529__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU23 --silicon_errata=CPU40 --preproc_with_compile --preproc_dependency="main.d" $(GEN_OPTS__FLAG) "$<"
+	"D:/Programs/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.3.LTS/bin/cl430" -vmspx --data_model=restricted --use_hw_mpy=F5 --include_path="D:/Programs/ti/ccsv7/ccs_base/msp430/include" --include_path="D:/Programs/Google Drive/2017 Fall (Junior 1)/Embedded Systems/GIT/lab-5-sensing-the-world-around-you-juice-crew-rev-1/Sensors and Signal Conditioning/MSP430F5529" --include_path="D:/Programs/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.3.LTS/include" --advice:power=all --define=__MSP430F5529__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU23 --silicon_errata=CPU40 --preproc_with_compile --preproc_dependency="main.d" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
